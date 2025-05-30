14e936e905213c9660cd4c5028be57a1ceb96ee3
Alexander Vorobyev <voral.adm@gmail.com>
2025-05-30 09:58:31 +0300

    feat: Добавил моки функций DeleteDirFiles, CopyDirFiles

--- src/functions/bx-functions.php
+++ src/functions/bx-functions.php
@@ -18,3 +18,22 @@ function GetMessage($name, $aReplace = null): string
 {
     return MockFunctions::executeMocked('GetMessage', [$name, $aReplace]);
 }
+
+function DeleteDirFiles(string $srcPath, string $dstPath, array $exclude = []): bool
+{
+    return MockFunctions::executeMocked('DeleteDirFiles', [$srcPath, $dstPath, $exclude]);
+}
+
+function CopyDirFiles(
+    $path_from,
+    $path_to,
+    $ReWrite = true,
+    $Recursive = false,
+    $bDeleteAfterCopy = false,
+    $strExclude = '',
+): bool {
+    return MockFunctions::executeMocked(
+        'CopyDirFiles',
+        [$path_from, $path_to, $ReWrite, $Recursive, $bDeleteAfterCopy, $strExclude],
+    );
+}
